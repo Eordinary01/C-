425b41adb0bee9856d6eb139bdbe68785c352b7d
Parth <parth.manocha2901@gmail.com>
2024-01-18 08:16:39 +0530

    initial commet

---
+++ loops.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int age;
+
+cout<<"Enter Your Age:";
+cin>>age;
+
+
+if (age>=18){
+    cout<<"You can vote!!"<<endl;
+}
+else{
+    cout<<"Vai Ghar jayoo!!"<<endl;
+}
+
+
+    return 0; 
+}
---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Parth The Dev"<<endl;
+    // cout<<"Parth The Dev\n";
+
+    int a,b,sum,n,i;
+
+    cout<<"Enter the number for Multiplication Table:";
+    cin>>n;
+
+    for(i=1;i<=10;i++){
+        cout<< n <<" * "<< i << " = " << n * i << endl;
+    }
+
+
+
+
+
+
+
+    // a=5;
+    // b=4;
+    // c=7;
+    // int sum=a+b+c;
+    // cout<<"The additon of a+b+c is:" <<sum;
+    // cout<<sum;
+
+
+
+    // Taking input from user:
+/*
+    cout <<"Type any positive number:";
+    cin >> a;
+    cout <<"Type other positive number:";
+    cin >> b;
+    sum = a+b;
+    cout <<"The sum is:" << sum;
+*/
+
+    
+
+
+//camelCase Notation
+
+// int marksInSubjects=69;
+// cout<<"\nThe marks of student is:"<<marksInSubjects;
+
+
+    
+    return 0;
+}
